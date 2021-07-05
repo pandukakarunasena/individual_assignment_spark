cec0df68091154e7505d11808c738c2dc9823799
panduka <kandyman777@gmail.com>
2021-07-05 15:22:53 +0530

    feat( User resource): add user resource with register and login

--- ncms/src/main/java/com/panduka/ncms/resources/UserResource.java
+++ ncms/src/main/java/com/panduka/ncms/resources/UserResource.java
@@ -1,10 +1,13 @@
 package com.panduka.ncms.resources;
 
 import com.panduka.ncms.auth.TokenGenerator;
+import com.panduka.ncms.dao.UserDAO;
 import com.panduka.ncms.dao.impl.UserDAOImpl;
 import com.panduka.ncms.entity.User;
 import com.panduka.ncms.exception.InvalidCredentialsException;
-import javax.persistence.UniqueConstraint;
+
+import com.panduka.ncms.services.hospital.UserService;
+import com.panduka.ncms.services.hospital.UserServiceImpl;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +15,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,30 +24,45 @@ import org.apache.logging.log4j.Logger;
     private static final Logger logger = LogManager.getLogger(UserDAOImpl.class);
 
     //register
-    @POST @Path("/register") @Consumes(MediaType.APPLICATION_JSON) @Produces(MediaType.APPLICATION_JSON) public void userSignIn(
+    @POST @Path("/register") @Consumes(MediaType.APPLICATION_FORM_URLENCODED) public Response userRegister(
             @FormParam("firstName") String firstName, @FormParam("lastName") String lastName,
             @FormParam("password") String password, @FormParam("role") String role,
             @FormParam("username") String username) {
-        //1. check for exiting users in database by the username
-        //if yes UserCreationFailed exception else
-        //create a user and send a JWT to login
+        if (username.isEmpty() || password.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || role.isEmpty()) {
+            return Response.status(401).entity("bad request").build();
+        }
+        try {
+            UserService userService = new UserServiceImpl();
+            User newUser = new User(username, password, firstName, lastName, role, null);
+            String token = userService.register(newUser);
+            Response.ResponseBuilder rb = Response.ok("token sent");
+            Response response = rb.header("Authorization", token).build();
+            return response;
+
+        } catch (InvalidCredentialsException ex) {
+            logger.error(ex.getMessage());
+            logger.error(ex.getCause());
+            ex.printStackTrace();
+            return Response.status(403).entity("user creation failed").build();
+        }
     }
 
     //login
+
     @Path("/login") @POST @Consumes(MediaType.APPLICATION_FORM_URLENCODED) public Response userLogin(
             @FormParam("password") String password, @FormParam("username") String username) {
         if (username.isEmpty() || password.isEmpty()) {
             return Response.status(401).entity("bad request").build();
         }
+        System.out.println(password);
+        System.out.println(username);
         try {
-            User user = new UserDAOImpl().getUserByUserNameAndPassword(username, password);
+            UserService userService = new UserServiceImpl();
+            String token = userService.login(username, password);
 
-            if (user == null) {
-                throw new InvalidCredentialsException();
-            }
-
-            String token = new TokenGenerator().generateToken(username, password);
-            return Response.status(200).entity(token).build();
+            Response.ResponseBuilder rb = Response.ok("token sent");
+            Response response = rb.header("Authorization", token).build();
+            return response;
 
         } catch (InvalidCredentialsException ex) {
             logger.error(ex.getMessage());
