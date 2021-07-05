fe285d52aa8fce4a5aaeb7a54ace6186619ba003
panduka <kandyman777@gmail.com>
2021-07-05 19:22:02 +0530

    config( ): filter configured

--- ncms/src/main/java/com/panduka/ncms/JerseyConfig.java
+++ ncms/src/main/java/com/panduka/ncms/JerseyConfig.java
@@ -2,23 +2,21 @@ package com.panduka.ncms;
 
 import static com.panduka.ncms.utils.Constants.PACKAGE_NAME_FOR_RESOURCES;
 
+import com.panduka.ncms.auth.AuthenticationFilter;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-
-@ApplicationPath("/api")
-public class JerseyConfig extends ResourceConfig {
+@ApplicationPath("/api") public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         packages(PACKAGE_NAME_FOR_RESOURCES);
         register(new LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME), Level.INFO,
                 LoggingFeature.Verbosity.PAYLOAD_ANY, 10000));
+        register(AuthenticationFilter.class);
     }
 
 }
